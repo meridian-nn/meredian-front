5f23d13c42c667dbce9bef6bd0e9ad3093fa85e5
Уткин Дмитрий <dreyk.avadon@gmail.com>
2021-03-16 22:56:59 +0300

    refactor add gtin element

--- views/vcrm/Dashboard/index.vue
+++ views/vcrm/Dashboard/index.vue
@@ -258,212 +258,24 @@
       </v-card>
     </v-dialog>
 
-    <v-dialog
-      v-model="dialog"
-      max-width="700px"
-    >
-      <template #activator="{ on, attrs }">
-        <v-fab-transition>
-          <v-btn
-            color="blue"
-            class="mr-2 mb-2"
-            fab
-            dark
-            small
-            fixed
-            bottom
-            right
-            v-bind="attrs"
-            v-on="on"
-          >
-            <v-icon>mdi-plus</v-icon>
-          </v-btn>
-        </v-fab-transition>
-      </template>
-
-      <v-card>
-        <v-card-title>
-          <span class="headline">{{ formTitle }}</span>
-        </v-card-title>
-
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col cols="6">
-                <v-text-field
-                  v-model="editedItem.categoryId"
-                  label="id категории"
-                />
-              </v-col>
-
-              <v-col cols="6">
-                <v-text-field
-                  v-model="editedItem.prodName"
-                  label="Название"
-                />
-              </v-col>
-
-              <v-col cols="6">
-                <v-text-field
-                  v-model="editedItem.brand"
-                  label="Брэнд"
-                />
-              </v-col>
-
-              <v-col cols="6">
-                <v-autocomplete
-                  v-model="editedItem.packType"
-                  :items="packTypeList"
-                  label="Тип упаковки"
-                />
-              </v-col>
-
-              <v-col cols="6">
-                <v-autocomplete
-                  v-model="editedItem.packMaterial"
-                  :items="materialList"
-                  label="Материал упаковки"
-                />
-              </v-col>
-
-              <v-col cols="6">
-                <v-text-field
-                  v-model="editedItem.quantity"
-                  type="number"
-                  label="Количество"
-                />
-              </v-col>
-
-              <v-col cols="6">
-                <v-text-field
-                  v-model="editedItem.companyName"
-                  label="Название компании"
-                />
-              </v-col>
-
-              <v-col cols="6">
-                <v-text-field
-                  v-model="editedItem.inn"
-                  label="Инн"
-                />
-              </v-col>
-
-              <v-col cols="6">
-                <v-text-field
-                  v-model="editedItem.tnved"
-                  label="Tnved"
-                />
-              </v-col>
-
-              <v-col cols="6">
-                <v-text-field
-                  v-model="editedItem.measure"
-                  label="Название количества"
-                />
-              </v-col>
-
-              <v-col cols="6">
-                <v-text-field
-                  v-model="editedItem.gcpclBrick"
-                  label="Категория по классификации GS1 GPC"
-                />
-              </v-col>
-
-              <v-col cols="6">
-                <v-autocomplete
-                  v-model="editedItem.kind"
-                  :items="typesList"
-                  label="Тип"
-                />
-              </v-col>
-
-              <v-col cols="6">
-                <v-text-field
-                  v-model="editedItem.sizeName"
-                  label="Название размера"
-                />
-              </v-col>
-
-              <v-col cols="6">
-                <v-text-field
-                  v-model="editedItem.sizeValue"
-                  label="Размер значения"
-                />
-              </v-col>
-
-              <v-col cols="6">
-                <v-autocomplete
-                  v-model="editedItem.color"
-                  :items="colorList"
-                  label="Цвет"
-                />
-              </v-col>
-
-              <v-col cols="6">
-                <v-text-field
-                  v-model="editedItem.model"
-                  label="Модель"
-                />
-              </v-col>
-
-              <v-col cols="6">
-                <v-select
-                  v-model="editedItem.gender"
-                  :items="genderList"
-                  label="Пол"
-                />
-              </v-col>
-
-              <v-col cols="6">
-                <v-text-field
-                  v-model="editedItem.standartNumber"
-                  label="Технический стандарт"
-                />
-              </v-col>
-
-              <v-col cols="6">
-                <v-text-field
-                  v-model="editedItem.rawMaterial"
-                  label="Материал"
-                />
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-card-text>
-
-        <v-card-actions>
-          <v-spacer />
-
-          <v-btn
-            color="blue darken-1"
-            text
-            @click="close"
-          >
-            Отмена
-          </v-btn>
-
-          <v-btn
-            color="blue darken-1"
-            text
-            :loading="loading"
-            @click="save"
-          >
-            Сохранить
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+    <element-add
+      show="dialog"
+      @close="close"
+      @save="save"
+    />
   </v-container>
 </template>
 
 <script>
 import omit from 'lodash/omit'
+import ElementAdd from '@/views/vcrm/Dashboard/Modals/ElementAdd'
 import MarkTable from './Mark'
 export default {
   name: 'DashboardPage',
 
   components: {
-    MarkTable
+    MarkTable,
+    ElementAdd
   },
 
   data: () => ({
@@ -820,63 +632,7 @@ export default {
       markCodeQuantity: ''
     },
 
-    markItem: {},
-
-    editedItem: {
-      categoryId: '31326',
-      prodName: '',
-      brand: '',
-      packType: '',
-      packMaterial: '',
-      quantity: '',
-      types: '',
-      sizeName: '',
-      sizeValue: '',
-      color: '',
-      country: 'RU',
-      kind: '',
-      gcpclBrick: '',
-      measure: '',
-      inn: '5259003336 (Меридиан)',
-      model: '',
-      gender: '',
-      standartNumber: '',
-      rawMaterial: '',
-      companyName: 'meridian',
-      manufacturerCode: '4650067329994',
-      tnved: '',
-      trademark: 'Меридиан',
-      apiExtension: 'lp',
-      status: 'NOT_PROCESSED'
-    },
-
-    defaultItem: {
-      categoryId: '31326',
-      prodName: '',
-      brand: '',
-      packType: '',
-      packMaterial: '',
-      quantity: '',
-      types: '',
-      sizeName: '',
-      sizeValue: '',
-      color: '',
-      country: 'RU',
-      kind: '',
-      gcpclBrick: '',
-      measure: '',
-      inn: '5259003336 (Меридиан)',
-      model: '',
-      gender: '',
-      standartNumber: '',
-      rawMaterial: '',
-      companyName: 'meridian',
-      manufacturerCode: '4650067329994',
-      tnved: '',
-      trademark: 'Меридиан',
-      apiExtension: 'lp',
-      status: 'NOT_PROCESSED'
-    }
+    markItem: {}
   }),
 
   async fetch() {
@@ -1067,7 +823,6 @@ export default {
     close() {
       this.dialog = false
       this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
         this.editedIndex = -1
       })
     },
@@ -1075,22 +830,15 @@ export default {
     closeDelete() {
       this.dialogDelete = false
       this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
         this.editedIndex = -1
       })
     },
 
-    async save() {
-      // if (this.editedIndex > -1) {
-      //   Object.assign(this.desserts[this.editedIndex], this.editedItem)
-      // } else {
-      //   this.desserts.push(this.editedItem)
-      // }
-
+    async save(data) {
       this.loading = true
 
       try {
-        await this.$axios.$post(`http://192.168.1.70:9037/gtinRequest/saveGtinRequest?${new URLSearchParams(this.editedIndex > -1 ? Object.assign(this.fullDesserts[this.editedIndex], this.editedItem) : this.editedItem).toString()}`)
+        await this.$axios.$post(`http://192.168.1.70:9037/gtinRequest/saveGtinRequest?${new URLSearchParams(this.editedIndex > -1 ? Object.assign(this.fullDesserts[this.editedIndex], data) : data).toString()}`)
 
         await this.$fetch()
 
