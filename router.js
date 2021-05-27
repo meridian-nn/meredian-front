bed40ce626e2277fc409a27f53140b3eb85e1fbf
Уткин Дмитрий <dreyk.avadon@gmail.com>
2021-05-27 11:41:52 +0300

    refact file structure

--- router.js
+++ router.js
@@ -31,185 +31,205 @@ const AuthLogin = dynamicPage(() =>
 const VcrmPage = dynamicPage(() =>
     import ('~/views/vcrm/VcrmPage'))
 
-const DashboardPage = dynamicPage(() =>
-    import ('~/views/vcrm/Dashboard'))
 
 const PaymentMenuPage = dynamicPage(() =>
-    import ('~/views/payment/Menu'))
+    import ('~/views/vcrm/Payment/Menu'))
 
 const JournalOfPaymentDocumentsPage = dynamicPage(() =>
-    import ('~/views/payment/PaymentDocument/JournalOfPaymentDocuments'))
+    import ('~/views/vcrm/Payment/PaymentDocument/JournalOfPaymentDocuments'))
 
 const JournalOfEmailSendingPaymentDocumentsPage = dynamicPage(() =>
-    import ('~/views/payment/PaymentDocument/JournalOfEmailSendingPaymentDocuments'))
+    import ('~/views/vcrm/Payment/PaymentDocument/JournalOfEmailSendingPaymentDocuments'))
 
 const PaymentByCashboxPage = dynamicPage(() =>
-    import ('~/views/payment/PaymentDocument/Modals/PaymentByCashbox.vue'))
+    import ('~/views/vcrm/Payment/PaymentDocument/Modals/PaymentByCashbox.vue'))
 
 const InternalPaymentPage = dynamicPage(() =>
-    import ('~/views/payment/PaymentDocument/Modals/InternalPayment.vue'))
+    import ('~/views/vcrm/Payment/PaymentDocument/Modals/InternalPayment.vue'))
 
 const HistoryOfPaymentDocumentsPage = dynamicPage(() =>
-    import ('~/views/payment/HistoryOfJournalOfPaymentDocuments'))
+    import ('~/views/vcrm/Payment/HistoryOfJournalOfPaymentDocuments'))
 
 const MoneyDistributionPage = dynamicPage(() =>
-    import ('~/views/payment/MoneyDistribution'))
+    import ('~/views/vcrm/Payment/MoneyDistribution'))
 
 const InputOfBalancesPage = dynamicPage(() =>
-    import ('~/views/payment/InputOfBalances'))
+    import ('~/views/vcrm/Payment/InputOfBalances'))
 
 const PaymentBudgetByDepartmentsPage = dynamicPage(() =>
-    import ('~/views/payment/PaymentBudgetByDepartments'))
+    import ('~/views/vcrm/Payment/PaymentBudgetByDepartments'))
 
 const RegisterOfDocumentsToPayPage = dynamicPage(() =>
-    import ('~/views/payment/RegisterOfDocumentsToPay'))
+    import ('~/views/vcrm/Payment/RegisterOfDocumentsToPay'))
 
 const RegisterOfPaymentsByCashboxPage = dynamicPage(() =>
-    import ('~/views/payment/RegisterOfPaymentsByCashbox'))
+    import ('~/views/vcrm/Payment/RegisterOfPaymentsByCashbox'))
 
 const GtinPage = dynamicPage(() =>
-    import ('~/views/vcrm/Dashboard/Gtin'))
+    import ('~/views/vcrm/Gtin/GtinPage'))
 
 const MarkPage = dynamicPage(() =>
-    import ('~/views/vcrm/Dashboard/Mark'))
+    import ('~/views/vcrm/Mark/MarkPage'))
 
 const ChatPage = dynamicPage(() =>
-    import ('~/views/vcrm/Dashboard/Chat/ChatPage'))
+    import ('~/views/vcrm/Chat/ChatPage'))
 
 Vue.use(Router)
 
 const createRouterConst = new Router({
     mode: 'history',
-
     scrollBehavior,
+    routes: [
+      {
+        path: '/meridian',
+        component: VcrmPage,
+        children: [
+          {
+            path: 'gtin',
+            name: 'GtinPage',
+            component: GtinPage,
+            meta: {
+              breadcrumb: [
+                {
+                  text: 'Профиль',
+                  disabled: false,
+                  href: 'Dashboard'
+                },
+                {
+                  text: 'Журнал GTIN',
+                  disabled: true,
+                  href: 'GtinPage'
+                }
+              ]
+            }
+          },
 
-    routes: [{
-            path: '/meridian',
-            component: VcrmPage,
-            children: [{
-                path: 'dashboard',
-                name: 'Dashboard',
-                component: DashboardPage,
-                children: [{
-                        path: 'gtin',
-                        name: 'GtinPage',
-                        component: GtinPage,
-                        meta: {
-                            breadcrumb: [
-                                { text: 'Профиль', disabled: false, href: 'Dashboard' },
-                                { text: 'Журнал GTIN', disabled: true, href: 'GtinPage' }
-                            ]
-                        }
-                    },
+          {
+            path: 'mark',
+            name: 'MarkPage',
+            component: MarkPage,
+            meta: {
+              breadcrumb: [
+                {
+                  text: 'Профиль',
+                  disabled: false,
+                  href: 'Dashboard'
+                },
+                {
+                  text: 'Журнал Кодов Маркировки',
+                  disabled: true,
+                  href: 'MarkPage'
+                }
+              ]
+            }
+          },
 
-                    {
-                        path: 'mark',
-                        name: 'MarkPage',
-                        component: MarkPage,
-                        meta: {
-                            breadcrumb: [
-                                { text: 'Профиль', disabled: false, href: 'Dashboard' },
-                                { text: 'Журнал Кодов Маркировки', disabled: true, href: 'MarkPage' }
-                            ]
-                        }
-                    },
+          {
+            path: 'chat',
+            name: 'ChatPage',
+            component: ChatPage,
+            meta: {
+              breadcrumb: [
+                {
+                  text: 'Профиль',
+                  disabled: false,
+                  href: 'Dashboard'
+                },
+                {
+                  text: 'Чат',
+                  disabled: true,
+                  href: 'ChatPage'
+                }
+              ]
+            }
+          },
 
-                    {
-                        path: 'chat',
-                        name: 'ChatPage',
-                        component: ChatPage,
-                        meta: {
-                            breadcrumb: [
-                                { text: 'Профиль', disabled: false, href: 'Dashboard' },
-                                { text: 'Чат', disabled: true, href: 'ChatPage' }
-                            ]
-                        }
-                    },
+          {
+            path: 'payment_menu',
+            name: 'PaymentMenu',
+            component: PaymentMenuPage,
+            meta: {
+              breadcrumb: [
+                {
+                  text: 'Меню реестра оплат',
+                  disabled: false
+                }
+              ]
+            }
+          },
 
-                    {
-                        path: 'payment_menu',
-                        name: 'PaymentMenu',
-                        component: PaymentMenuPage,
-                        meta: {
-                            breadcrumb: [
-                                { text: 'Меню реестра оплат', disabled: false }
-                            ]
-                        }
-                    },
+          {
+            path: 'journal_of_payment_documents',
+            name: 'JournalOfPaymentDocuments',
+            component: JournalOfPaymentDocumentsPage
+          },
 
-                    {
-                        path: 'journal_of_payment_documents',
-                        name: 'JournalOfPaymentDocuments',
-                        component: JournalOfPaymentDocumentsPage
-                    },
+          {
+            path: 'payment_by_cashbox',
+            name: 'PaymentByCashbox',
+            component: PaymentByCashboxPage
+          },
 
-                    {
-                        path: 'payment_by_cashbox',
-                        name: 'PaymentByCashbox',
-                        component: PaymentByCashboxPage
-                    },
+          {
+            path: 'internal_payment',
+            name: 'InternalPayment',
+            component: InternalPaymentPage
+          },
 
-                    {
-                        path: 'internal_payment',
-                        name: 'InternalPayment',
-                        component: InternalPaymentPage
-                    },
+          {
+            path: 'payment_budget_by_departmens',
+            name: 'PaymentBudgetByDepartments',
+            component: PaymentBudgetByDepartmentsPage
+          },
 
-                    {
-                        path: 'payment_budget_by_departmens',
-                        name: 'PaymentBudgetByDepartments',
-                        component: PaymentBudgetByDepartmentsPage
-                    },
+          {
+            path: 'journal_of_email_sending_payment_documents',
+            name: 'JournalOfEmailSendingPaymentDocuments',
+            component: JournalOfEmailSendingPaymentDocumentsPage
+          },
 
-                    {
-                        path: 'journal_of_email_sending_payment_documents',
-                        name: 'JournalOfEmailSendingPaymentDocuments',
-                        component: JournalOfEmailSendingPaymentDocumentsPage
-                    },
+          {
+            path: 'history_of_payment_documents',
+            name: 'HistoryOfPaymentDocuments',
+            component: HistoryOfPaymentDocumentsPage
+          },
 
-                    {
-                        path: 'history_of_payment_documents',
-                        name: 'HistoryOfPaymentDocuments',
-                        component: HistoryOfPaymentDocumentsPage
-                    },
+          {
+            path: 'money_distribution',
+            name: 'MoneyDistribution',
+            component: MoneyDistributionPage
+          },
 
-                    {
-                        path: 'money_distribution',
-                        name: 'MoneyDistribution',
-                        component: MoneyDistributionPage
-                    },
+          {
+            path: 'input_of_balances',
+            name: 'InputOfBalances',
+            component: InputOfBalancesPage
+          },
 
-                    {
-                        path: 'input_of_balances',
-                        name: 'InputOfBalances',
-                        component: InputOfBalancesPage
-                    },
+          {
+            path: 'register_of_documents_to_pay',
+            name: 'RegisterOfDocumentsToPay',
+            component: RegisterOfDocumentsToPayPage
+          },
 
-                    {
-                        path: 'register_of_documents_to_pay',
-                        name: 'RegisterOfDocumentsToPay',
-                        component: RegisterOfDocumentsToPayPage
-                    },
+          {
+            path: 'register_of_payments_by_cashbox',
+            name: 'RegisterOfPaymentsByCashbox',
+            component: RegisterOfPaymentsByCashboxPage
+          },
 
-                    {
-                        path: 'register_of_payments_by_cashbox',
-                        name: 'RegisterOfPaymentsByCashbox',
-                        component: RegisterOfPaymentsByCashboxPage
-                    },
-
-                ]
-            }]
-        },
-        {
+          {
             path: '/meridian/auth',
             name: 'Auth',
             component: AuthPage,
             children: [{
-                path: 'login',
-                name: 'Login',
-                component: AuthLogin
+              path: 'login',
+              name: 'Login',
+              component: AuthLogin
             }]
-        }
+          }
+        ]
+      }
     ].filter((route) => {
         return process.env.NODE_ENV !== 'production' ? route : !('production' in route)
     })
