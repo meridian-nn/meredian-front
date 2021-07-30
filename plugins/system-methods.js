218bb21f8f3a4c05d3f7040b5b3f07a6ab961c7c
Vladimir Dontsov <Vova-moove@yandex.ru>
2021-07-30 15:11:06 +0300

    Доработаны формы "Редактирование_заказа_на_дополнительную_работу" и "Редактирование_Заказа_На_Пошив"

--- plugins/system-methods.js
+++ plugins/system-methods.js
@@ -1,155 +1,170 @@
 /* eslint-disable */
 // Плагин c разными системными методами
 import Vue from 'vue'
+
 Vue.mixin({
-    methods: {
-        //Функция возвращает id таблицы "Документы на оплату" формы "Журнал документов на оплату"
-        getIdOfFromPayDocsTableOfJournalOfPaymentDocs() {
-            return 'journal-of-payment-docs-from-pay-docs'
-        },
-        getIdOfFromOutgoingDocumentsTable() {
-          return 'outgoing-payment-document'
-        },
-        getIdOfRecordsTableOfRecordsOfWorkByCards() {
-            return 'records-of-work-by-cards-table-of-records'
-        },
+  methods: {
+    //Функция возвращает id таблицы "Документы на оплату" формы "Журнал документов на оплату"
+    getIdOfFromPayDocsTableOfJournalOfPaymentDocs() {
+      return 'journal-of-payment-docs-from-pay-docs'
+    },
+    getIdOfFromOutgoingDocumentsTable() {
+      return 'outgoing-payment-document'
+    },
+    getIdOfRecordsTableOfRecordsOfWorkByCards() {
+      return 'records-of-work-by-cards-table-of-records'
+    },
 
-        //Функция возвращает form id и element id для orgId и accId по умолчанию
-        getObjectWithFormIdAndElementIdForDefaultOrgAndAcc() {
-          return {
-            formId: 'everywhere',
-            elementId: 'everywhere'
-          }
-        },
+    //Функция возвращает form id и element id для orgId и accId по умолчанию
+    getObjectWithFormIdAndElementIdForDefaultOrgAndAcc() {
+      return {
+        formId: 'everywhere',
+        elementId: 'everywhere'
+      }
+    },
 
-      // Функция поиска организации и расчетного счета по умолчанию для текущего пользователя
-        async findDefaultOrgAndAccIdForUser() {
-          const formAndElementIdsOfOrgAndAccIds = this.getObjectWithFormIdAndElementIdForDefaultOrgAndAcc()
-          const dataForFiltersQuery = this.createCriteriasToSearchForFiltersValues(formAndElementIdsOfOrgAndAccIds.formId,
-            formAndElementIdsOfOrgAndAccIds.elementId, this.getCurrentUser.id)
-          const response = await this.$api.uiSettings.findBySearchCriterias(dataForFiltersQuery)
-          let filtersParams = {}
+    // Функция поиска организации и расчетного счета по умолчанию для текущего пользователя
+    async findDefaultOrgAndAccIdForUser() {
+      const formAndElementIdsOfOrgAndAccIds = this.getObjectWithFormIdAndElementIdForDefaultOrgAndAcc()
+      const dataForFiltersQuery = this.createCriteriasToSearchForFiltersValues(formAndElementIdsOfOrgAndAccIds.formId,
+        formAndElementIdsOfOrgAndAccIds.elementId, this.getCurrentUser.id)
+      const response = await this.$api.uiSettings.findBySearchCriterias(dataForFiltersQuery)
+      let filtersParams = {}
 
-          if (response.length) {
-            filtersParams = JSON.parse(response[0].settingValue)
-          }
+      if (response.length) {
+        filtersParams = JSON.parse(response[0].settingValue)
+      }
 
-          return filtersParams
-        },
+      return filtersParams
+    },
 
-        //Функция возвращает цвет организации по переданному id организации
-        getColorForOrganization(orgId) {
-            if (orgId === 159) {
-                return '#639db1'
-            } else {
-                return this.getRandomColor()
-            }
-        },
+    //Функция возвращает цвет организации по переданному id организации
+    getColorForOrganization(orgId) {
+      if (orgId === 159) {
+        return '#639db1'
+      } else {
+        return this.getRandomColor()
+      }
+    },
 
-        getRandomColor() {
-          const letters = '0123456789ABCDEF'
-          let color = '#'
-          for (let i = 0; i < 6; i++) {
-                color += letters[Math.floor(Math.random() * 16)];
-            }
-            return color;
-        },
+    getRandomColor() {
+      const letters = '0123456789ABCDEF'
+      let color = '#'
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
+      }
+      return color
+    },
 
-        //Функция возвращает организации, у которых sp_org_type_id = 1
-        async getBudgetOrganizations() {
-          const data = {
-            typeCode: 1
-          }
+    //Функция возвращает организации, у которых sp_org_type_id = 1
+    async getBudgetOrganizations() {
+      const data = {
+        typeCode: 1
+      }
 
-          const organizations = await this.$api.organizations.findByOrgTypeCode(data)
-          organizations[1] = organizations.splice(0, 1, organizations[1])[0]
+      const organizations = await this.$api.organizations.findByOrgTypeCode(data)
+      organizations[1] = organizations.splice(0, 1, organizations[1])[0]
 
-          return organizations
-        },
+      return organizations
+    },
 
-        getDateForCriteriasToSearchDocsFromPay() {
-          let currentDate = new Date()
-          return new Date(currentDate.getFullYear() - 1, currentDate.getMonth(), currentDate.getDate())
-        },
+    getDateForCriteriasToSearchDocsFromPay() {
+      let currentDate = new Date()
+      return new Date(currentDate.getFullYear() - 1, currentDate.getMonth(), currentDate.getDate())
+    },
 
-        // Функция возвращает текущую дату минус 6 месяцев
-        getDateForCriteriasToSearchExecutorsByDataUvol() {
-          let currentDate = new Date()
-          return new Date(currentDate.getFullYear(), currentDate.getMonth() - 6, currentDate.getDate())
-        },
+    // Функция возвращает текущую дату минус 6 месяцев
+    getDateForCriteriasToSearchExecutorsByDataUvol() {
+      let currentDate = new Date()
+      return new Date(currentDate.getFullYear(), currentDate.getMonth() - 6, currentDate.getDate())
+    },
 
-        convertLocaleDateStringToDate(date) {
-          const parts = date.split('.')
-          return new Date(parts[2], parts[1] - 1, parts[0])
-        },
+    convertLocaleDateStringToDate(date) {
+      const parts = date.split('.')
+      return new Date(parts[2], parts[1] - 1, parts[0])
+    },
 
-        async changeSumToPayOfPaymentAccount(accId, sumOfPaymentDocs, operationType, date) {
-          if(sumOfPaymentDocs === 0) {
-            return
-          }
+    async changeSumToPayOfPaymentAccount(accId, sumOfPaymentDocs, operationType, date) {
+      if (sumOfPaymentDocs === 0) {
+        return
+      }
 
-          const searchCriterias = this.createCriteriasToFindPaymentAccount(accId,date)
-          const response = await this.$api.paymentAccounts.findBySearchCriteriaList(searchCriterias)
-          let paymentAccount
+      const searchCriterias = this.createCriteriasToFindPaymentAccount(accId, date)
+      const response = await this.$api.paymentAccounts.findBySearchCriteriaList(searchCriterias)
+      let paymentAccount
 
-          if(!response) {
-            return
-          } else {
-            paymentAccount = response[0]
-          }
+      if (!response) {
+        return
+      } else {
+        paymentAccount = response[0]
+      }
 
-          paymentAccount.sumToPay = paymentAccount.sumToPay ? paymentAccount.sumToPay : 0
+      paymentAccount.sumToPay = paymentAccount.sumToPay ? paymentAccount.sumToPay : 0
 
-          if(operationType === 'SUM') {
-              paymentAccount.sumToPay = paymentAccount.sumToPay + sumOfPaymentDocs
-          } else if(operationType === 'DEDUCT') {
-            paymentAccount.sumToPay = paymentAccount.sumToPay - sumOfPaymentDocs
-          }
+      if (operationType === 'SUM') {
+        paymentAccount.sumToPay = paymentAccount.sumToPay + sumOfPaymentDocs
+      } else if (operationType === 'DEDUCT') {
+        paymentAccount.sumToPay = paymentAccount.sumToPay - sumOfPaymentDocs
+      }
 
-          return await this.$api.paymentAccounts.save(paymentAccount)
-        },
+      return await this.$api.paymentAccounts.save(paymentAccount)
+    },
 
-        async changeVnplOfPaymentAccounts(accIdOfPayer, accIdOfReceiver, sumOfPayment, dateOfDoc) {
-          if(sumOfPayment === 0) {
-            return
-          }
+    async changeVnplOfPaymentAccounts(accIdOfPayer, accIdOfReceiver, sumOfPayment, dateOfDoc) {
+      if (sumOfPayment === 0) {
+        return
+      }
 
-          const searchCriterias = this.createCriteriasToFindTwoPaymentAccounts(accIdOfPayer, accIdOfReceiver, dateOfDoc)
-          const response = await this.$api.paymentAccounts.findBySearchCriteriaList(searchCriterias)
-          let paymentAccountOfPayer
-          let paymentAccountOfReceiver
+      const searchCriterias = this.createCriteriasToFindTwoPaymentAccounts(accIdOfPayer, accIdOfReceiver, dateOfDoc)
+      const response = await this.$api.paymentAccounts.findBySearchCriteriaList(searchCriterias)
+      let paymentAccountOfPayer
+      let paymentAccountOfReceiver
 
-          if (!response) {
-            return
-          } else {
-            paymentAccountOfPayer = response.find(el => el.acc.id === accIdOfPayer)
-            paymentAccountOfReceiver = response.find(el => el.acc.id === accIdOfReceiver)
-          }
+      if (!response) {
+        return
+      } else {
+        paymentAccountOfPayer = response.find(el => el.acc.id === accIdOfPayer)
+        paymentAccountOfReceiver = response.find(el => el.acc.id === accIdOfReceiver)
+      }
 
-          if(!paymentAccountOfPayer || !paymentAccountOfReceiver) {
-            return
-          }
+      if (!paymentAccountOfPayer || !paymentAccountOfReceiver) {
+        return
+      }
 
-          paymentAccountOfPayer.vnpl = paymentAccountOfPayer.vnpl ? paymentAccountOfPayer.vnpl : 0
-          paymentAccountOfPayer.vnpl = paymentAccountOfPayer.vnpl - sumOfPayment
+      paymentAccountOfPayer.vnpl = paymentAccountOfPayer.vnpl ? paymentAccountOfPayer.vnpl : 0
+      paymentAccountOfPayer.vnpl = paymentAccountOfPayer.vnpl - sumOfPayment
 
-          paymentAccountOfReceiver.vnpl = paymentAccountOfReceiver.vnpl ? paymentAccountOfReceiver.vnpl : 0
-          paymentAccountOfReceiver.vnpl = paymentAccountOfReceiver.vnpl + sumOfPayment
+      paymentAccountOfReceiver.vnpl = paymentAccountOfReceiver.vnpl ? paymentAccountOfReceiver.vnpl : 0
+      paymentAccountOfReceiver.vnpl = paymentAccountOfReceiver.vnpl + sumOfPayment
 
-          const arrayOfPaymentAccountsForSave = [
-            paymentAccountOfPayer,
-            paymentAccountOfReceiver
-          ]
+      const arrayOfPaymentAccountsForSave = [
+        paymentAccountOfPayer,
+        paymentAccountOfReceiver
+      ]
 
-          await this.$api.payment.balanceOfPaymentAccount.saveAll(arrayOfPaymentAccountsForSave)
-        },
+      await this.$api.payment.balanceOfPaymentAccount.saveAll(arrayOfPaymentAccountsForSave)
+    },
 
-        getConfigForDeleteMethods(){
-          return {
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          }
+    getConfigForDeleteMethods() {
+      return {
+        headers: {
+          'Content-Type': 'application/json'
         }
+      }
+    },
+
+    /* Remove the second item from the array
+    array.remove(1);
+    Remove the second-to-last item from the array
+    array.remove(-2);
+    Remove the second and third items from the array
+    array.remove(1,2);
+    Remove the last and second-to-last items from the array
+    array.remove(-2,-1); */
+    deleteElemFromArray(array, from, to) {
+      const rest = array.slice((to || from) + 1 || array.length)
+      array.length = from < 0 ? array.length + from : from
+      return array.push.apply(array, rest)
     }
+  }
 })
---
+++ views/vcrm/TechnologyOfProduction/SewingOrderLog/modals/EditOrderForTailoring.vue
@@ -0,0 +1,322 @@
+<template>
+  <v-dialog
+    :value="value"
+    :fullscreen="true"
+    class="edit-order-for-tailoring"
+    @input="$emit('close')"
+  >
+    <v-card>
+      <v-card-title>
+        <span class="headline">Редактирование заказа на пошив</span>
+      </v-card-title>
+
+      <v-card-text>
+        <div class="edit-order-for-tailoring-row">
+          <div class="edit-order-for-tailoring-cut">
+            <form-control label="Раскрой">
+              <v-autocomplete
+                v-model="editedItem.raskroy"
+              />
+            </form-control>
+          </div>
+
+          <form-control label="Плановая дата раскроя">
+            <v-text-field
+              v-model="editedItem.planingDateOfRaskroy"
+              type="date"
+            />
+          </form-control>
+        </div>
+
+        <div class="edit-order-for-tailoring-row">
+          <div class="edit-order-for-tailoring-kroy">
+            <form-control label="Крой">
+              <v-text-field
+                v-model="editedItem.kroy"
+                outlined
+                dense
+                hide-details="auto"
+              />
+            </form-control>
+          </div>
+
+          <div class="edit-order-for-tailoring-spacer" />
+
+          <div class="edit-order-for-tailoring-descr">
+            <v-textarea
+              v-model="editedItem.descr"
+              solo
+            />
+          </div>
+        </div>
+
+        <div class="edit-order-for-tailoring-row">
+          <div class="edit-order-for-tailoring-left-col">
+            <div class="edit-order-for-tailoring-contract">
+              <form-control label="Договор №">
+                <v-autocomplete
+                  v-model="editedItem.contract"
+                />
+              </form-control>
+            </div>
+
+            <div class="edit-order-for-tailoring-row">
+              <div class="edit-order-for-tailoring-date-of-order-for-tailoring">
+                <form-control label="Дата заказа на пошив">
+                  <v-text-field
+                    v-model="editedItem.dateOfTheOrderForTailoring"
+                    type="date"
+                  />
+                </form-control>
+              </div>
+
+              <v-simple-checkbox
+                v-model="editedItem.kroyOnPaper"
+              />
+
+              <div
+                class="edit-order-for-tailoring-label-checkbox"
+              >
+                Крой на бумаге
+              </div>
+            </div>
+
+            <div class="edit-order-for-tailoring-textarea-note">
+              <v-textarea
+                v-model="editedItem.note"
+                solo
+                label="Примечание"
+              />
+            </div>
+          </div>
+
+          <div class="edit-order-for-tailoring-spacer" />
+
+          <div class="edit-order-for-tailoring-right-col">
+            <div class="edit-order-for-tailoring-coefficient-for-tailoring">
+              <form-control label="Коэффициент на пошив">
+                <v-text-field
+                  v-model="editedItem.coefficientForTailoring"
+                  outlined
+                  dense
+                  hide-details="auto"
+                />
+              </form-control>
+            </div>
+
+            <div class="edit-order-for-tailoring-coefficient-for-raskroy">
+              <form-control label="Коэффициент на раской">
+                <v-text-field
+                  v-model="editedItem.coefficientForRaskroy"
+                  outlined
+                  dense
+                  hide-details="auto"
+                />
+              </form-control>
+            </div>
+
+            <div class="edit-order-for-tailoring-date-of-release">
+              <form-control label="Дата выпуска">
+                <v-text-field
+                  v-model="editedItem.dateOfRelease"
+                  type="date"
+                />
+              </form-control>
+            </div>
+
+            <v-checkbox
+              v-model="editedItem.allowedToPrintOnFactory"
+              label="Разрешено печатать на фабриках"
+            />
+          </div>
+        </div>
+
+        <div class="edit-order-for-tailoring-row">
+          <v-subheader
+            class="font-weight-medium text-subtitle-1"
+            style="margin: 0 auto"
+          >
+            Вышивка
+          </v-subheader>
+        </div>
+
+        <div class="edit-order-for-tailoring-row">
+          <vue-excel-editor
+            v-model="jsondata"
+          >
+            <vue-excel-column
+              field="kod"
+              label="Код"
+              type="string"
+              width="200px"
+            />
+            <vue-excel-column
+              field="name"
+              label="Наименование"
+              type="string"
+              width="1200px"
+            />
+            <vue-excel-column
+              field="quantity"
+              label="Количество"
+              type="string"
+              width="200px"
+            />
+            <vue-excel-column
+              field="order"
+              label="№ Заказа"
+              type="string"
+              width="200px"
+            />
+          </vue-excel-editor>
+        </div>
+
+        <div class="edit-order-for-tailoring-row">
+          <v-spacer />
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="close"
+          >
+            Закрыть
+          </v-btn>
+        </div>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'ModalEdit',
+
+  props: {
+    value: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  data() {
+    return {
+      editedItem: {
+        num_plan: '',
+        num_zkz: '',
+        kroy: 0,
+        descr: '',
+        raskroy: '',
+        planingDateOfRaskroy: new Date().toISOString().substr(0, 10),
+        contract: '',
+        kroyOnPaper: false,
+        dateOfTheOrderForTailoring: new Date().toISOString().substr(0, 10),
+        note: '',
+        coefficientForTailoring: 0,
+        coefficientForRaskroy: 0,
+        dateOfRelease: new Date().toISOString().substr(0, 10),
+        allowedToPrintOnFactory: false
+      },
+      jsondata: [
+        {
+          kod: '',
+          name: '',
+          quantity: 25,
+          order: ''
+        },
+        {
+          kod: '',
+          name: '',
+          quantity: 20,
+          order: ''
+        },
+        {
+          kod: '',
+          name: '',
+          quantity: 19,
+          order: ''
+        }
+      ],
+      formOpened: this.value
+    }
+  },
+
+  methods: {
+    close() {
+      this.formOpened = false
+      this.$emit('close')
+    }
+  }
+}
+</script>
+<style lang="scss">
+.edit-order-for-tailoring-main-div {
+  padding: 10px;
+
+  .row + .row {
+    margin-top: 0 !important;
+  }
+}
+
+.edit-order-for-tailoring-row {
+  display: flex;
+  flex-wrap: wrap;
+  flex: 1 1 auto;
+  min-width: 100%;
+}
+
+.edit-order-for-tailoring-cut {
+  width: 400px;
+}
+
+.edit-order-for-tailoring-contract {
+  width: 400px;
+}
+
+.edit-order-for-tailoring-kroy {
+  width: 10%;
+}
+
+.edit-order-for-tailoring-spacer {
+  width: 2%
+}
+
+.edit-order-for-tailoring-descr {
+  width: 88%;
+}
+
+.edit-order-for-tailoring-left-col {
+  flex: 0 0 78%;
+  max-width: 78%;
+}
+
+.edit-order-for-tailoring-right-col {
+  flex: 0 0 20%;
+  max-width: 20%;
+}
+
+.edit-order-for-tailoring-label-checkbox {
+  color: rgba(0, 0, 0, 0.6);
+  font-size: 13px;
+  padding-top: 24px;
+  font-weight: bold;
+}
+
+.edit-order-for-tailoring-date-of-order-for-tailoring {
+  width: 330px;
+}
+
+.edit-order-for-tailoring-textarea-note {
+  width: 100%;
+}
+
+.edit-order-for-tailoring-coefficient-for-tailoring {
+  margin-bottom: 10px;
+}
+
+.edit-order-for-tailoring-coefficient-for-raskroy {
+  margin-bottom: 10px;
+}
+
+.edit-order-for-tailoring-date-of-release {
+  margin-bottom: 10px;
+}
+</style>
